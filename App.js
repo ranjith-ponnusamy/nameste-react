7052da132b82b1a75ca6c4b801ee625c965dd147
Ranjith <ranjithponnusamy77@gmail.com>
2026-01-13 13:44:33 +0530

    Episode 3 of nameste react

--- App.js
+++ App.js
@@ -1,20 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  [
-    React.createElement("div", { id: "child1", key: "c1" }, [
-      React.createElement("h1", { key: "h1-1" }, "I am h1 tag inside child1"),
-      React.createElement("h2", { key: "h1-2" }, "i am ranjith I am h2 tag inside child1"),
-    ]),
-    React.createElement("div", { id: "child2", key: "c2" }, [
-      React.createElement("h1", { key: "h2-1" }, "I am h1 tag inside child2"),
-      React.createElement("h2", { key: "h2-2" }, "I am h2 tag inside child2"),
-    ])
-  ]
-);
+// const parent = React.createElement();
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(parent);
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+const Title = () => <h1>Hello from Title Component</h1>;
+
+const Heading = () => 
+<div className="parent">
+  <Title />
+  <h1 id="title" key="h1" className="heading" tabIndex={1}>Hello World from React</h1>
+</div >;
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(<Title />);
